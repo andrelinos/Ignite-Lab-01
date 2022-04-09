dc48ecff2ee1c6e42af720e962ed57d90995747d
Andrelino Silva <andrelinodev@gmail.com>
2022-04-09 15:36:41 -0300

    Aula 03

--- purchases/src/http/graphql/models/customer.ts
+++ purchases/src/http/graphql/models/customer.ts
@@ -1,10 +1,14 @@
-import { Field, ID, ObjectType } from '@nestjs/graphql';
+import { Directive, Field, ID, ObjectType } from '@nestjs/graphql';
 import { Purchase } from './purchase';
 
-@ObjectType()
+// Esta configuração interliga os microsserviços
+@ObjectType('User')
+@Directive('@key(fields: "authUserId")')
 export class Customer {
-  @Field(() => ID)
   id: string;
+
+  @Field(() => ID)
+  authUserId: string;
 
   @Field(() => [Purchase])
   purchases: Purchase[];
